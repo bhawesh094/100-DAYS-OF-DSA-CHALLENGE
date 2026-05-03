a921f816b06899662d4a2b6ffd20b804d7beedb0
bhawesh <rautelabhawesh44@gmail.com>
2026-05-03 14:17:17 +0530

    Add files via upload

---
+++ DAY-92.c
@@ -0,0 +1,59 @@
+/*Problem: Implement Quick Sort using partition method (Lomuto or Hoare).*/
+
+#include <stdio.h>
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];   // last element as pivot
+    int i = low - 1;
+    int j, temp;
+
+    for (j = low; j < high; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // place pivot in correct position
+    temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+}
+
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
